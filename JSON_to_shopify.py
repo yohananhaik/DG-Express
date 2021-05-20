591a6417be6257a6dd96f9836577453c19777e7a
Yohanan Haik <49283609+israweb@users.noreply.github.com>
2021-05-20 16:29:40 +0300

    create smart collection for each new group

--- JSON_to_shopify.py
+++ JSON_to_shopify.py
@@ -22,51 +22,19 @@ def get_all_products():
     return response.text
 
 def create_new_product(product):
-    new_product_url = url+ "products.json"
+    if get_product_id_by_title(product["product"]["title"]) ==0:
+        new_product_url = url+ "products.json"
 
-    payload= json.dumps(product)
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    
-    response = requests.request("POST", new_product_url, headers=headers, data=payload)
-    return(response.json())
-
-def is_collection_exist(collection_name):
-    retrieve_url = url+"custom_collections.json?title={}".format(collection_name)
-    payload={}
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    response = requests.request("GET", retrieve_url, headers=headers, data=payload)
-    collections = response.json()
-    if len(collections['custom_collections'])>0:
-        for collection in collections['custom_collections']:
-            if collection['title']==collection_name:
-                return collection['id']
-    return 0
-
-def create_new_collection(collection_name):
-    collection_id =  is_collection_exist(collection_name)
-    if collection_id:
-        return collection_id
-    else:
-        collection_url =url+ "custom_collections.json"
-
-        payload = json.dumps({
-        "custom_collection": {
-            "title": collection_name
-        }
-        })
+        payload= json.dumps(product)
         headers = {
         'Content-Type': 'application/json'
         }
-        response = requests.request("POST", collection_url, headers=headers, data=payload)
-        return response.json()['custom_collection']['id']
+        
+        response = requests.request("POST", new_product_url, headers=headers, data=payload)
+        return(response.json())
 
-def add_product_to_collection(product_id, collection_id):
+def add_product_to_custom_collection(product_id, collection_id):
     collect_url = url+"collects.json"
-
     payload = json.dumps({
     "collect": {
         "product_id": product_id,
@@ -78,15 +46,59 @@ def add_product_to_collection(product_id, collection_id):
     }
     response = requests.request("POST", collect_url, headers=headers, data=payload)
 
-def remove_item_from_collection(product_name):
+def remove_item_from_custom_collection(product_name):
     product_id = get_product_id_by_title(product_name)
     del_url = url+"collects/{}.json".format(product_id)
     payload = ""
     headers = {
     'Content-Type': 'application/json'
     }
-
     response = requests.request("DELETE", url, headers=headers, data=payload)
+
+def is_collection_exist(collection_name,type):
+    retrieve_url = url+"{}_collections.json?title={}".format(type,collection_name)
+    payload={}
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = requests.request("GET", retrieve_url, headers=headers, data=payload)
+    collections = response.json()
+    if len(collections['{}_collections'.format(type)])>0:
+        for collection in collections['{}_collections'.format(type)]:
+            if collection['title']==collection_name:
+                return collection['id']
+    return 0
+
+def create_new_collection(collection_name,type):
+    collection_id =  is_collection_exist(collection_name,type)
+    if collection_id:
+        return collection_id
+    else:
+        collection_url =url+ "{}_collections.json".format(type)
+        if type == 'custom':
+            payload = json.dumps({
+            "{}_collection".format(type): {
+                "title": collection_name
+            }})
+        elif type == 'smart':
+            payload = json.dumps({
+            "{}_collection".format(type): {
+                "title": collection_name,
+                "rules":[{
+                        "column": "type",
+                        "relation": "contains",
+                        "condition": collection_name
+                    }]
+            }})
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", collection_url, headers=headers, data=payload)
+        return response.json()['{}_collection'.format(type)]['id']
+
+def update_smart_collections(groups):
+    for category in groups:
+        create_new_collection(category,'smart')
 
 def get_product_id_by_title(product_title):
     retrieve_url = url+"products.json".format(product_title)
@@ -96,15 +108,16 @@ def get_product_id_by_title(product_title):
     }
     params = {'title':product_title}
     response = requests.request("GET", retrieve_url, headers=headers, data=payload,params=params)
-    return response.json()['products'][0]['id']
+    res = response.json()
+    if len(res['products'])>0:
+        return response.json()['products'][0]['id']
+    return 0
 
 def main():
-    collection_id = create_new_collection('test_collffhection')
     with open(config['mssql']['json_name'] ,'r') as file:
         items = json.load(file)
+        update_smart_collections(items["groups"])
         for product in items["products"]:
             response = create_new_product(product)
-            product_id = response['product']['id']
-            add_product_to_collection(product_id,collection_id)
 
 main()
--- RDT_to_JSON.py
+++ RDT_to_JSON.py
@@ -14,22 +14,78 @@ conn = pymssql.connect(server=mssql_credentials.get('server'),
                       password=mssql_credentials.get('password'), 
                       database=mssql_credentials.get('database'))
 cur = conn.cursor()
-
+groups = set()
 products = {"products":[]}
 
+def get_type_name(code):
+  query = "select ItemTypeName from ItemMainAndStoreView where BarcodeNumber ='{}'".format(code)
+  cur.execute(query)
+  return cur.fetchone()[0]
+
+def get_sku_from_parents(parents):
+      query = """SELECT DISTINCT I.BarcodeNumber [UPC] FROM ItemMainAndStoreView I 
+                  WHERE I.LinkNo IN (SELECT ItemID FROM ItemMainAndStoreView WHERE BarcodeNumber IN {}) 
+                  ORDER BY BarcodeNumber""".format(str(parents).replace('[','(').replace(']',')'))
+      cur.execute(query)
+      skus = [sku[0] for sku in cur.fetchall()]
+      return skus
+
+def get_barcode_numbers():
+      codes = mssql_credentials.get('sku').split(',')
+      parents=[]
+      sku=[]
+      for code in codes:
+            type_name = get_type_name(code)
+            if type_name == 'Parent':
+                  parents.append(code)
+            elif type_name == 'SKU':
+                  sku.append(code)
+      sku.extend(get_sku_from_parents(parents))
+      return sku
+
+
 def select_rows(row_num=0):
-  query = "SELECT {} Name,Description,Brand, Price,BarcodeNumber, OnHand,Size,Groups FROM dbo.ItemMainAndStoreView ;".format("TOP "+str(row_num) if row_num>0 else "*")
+  barcodes = get_barcode_numbers()
+  # query = "SELECT {} Name,Description,Brand, Price,BarcodeNumber, OnHand,Size,'' as Color,'' as Weight,Groups FROM dbo.ItemMainAndStoreView ;".format("TOP "+str(row_num) if row_num>0 else "*")
+  query = """select distinct 
+        ItemGroupName = STUFF((
+                  SELECT distinct ',' + G.ItemGroupName
+                  FROM itemgroup G
+        JOIN ItemtoGroup IG ON G.ItemGroupID = IG.ItemGroupID
+                  WHERE I.ItemStoreID = IG.ItemStoreID
+                  FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(500)'), 1, 1, '')
+        ,I.[Name]
+        ,I.[Description]
+        ,I.Brand
+        ,I.Price
+        ,I.BarcodeNumber [UPC]
+        ,I.OnHand
+        ,NULL [Color]
+        ,NULL [weight]
+        ,I.Matrix2 [size] 
+        from 
+        ItemMainAndStoreView I 
+        where I.BarcodeNumber in {}
+        order by BarcodeNumber""".format(str(barcodes).replace('[','(').replace(']',')'))
   cur.execute(query)
   return cur
 
+
+
 def create_product_object(row):
-  name,description,brand, price,sku, onHand,size,groups = row
+  groups,name,description,brand, price,sku, onHand,color,weight,size = row
+  options = []
+  if size:
+        options.append({"name": "Size","values": size })
+  if color:
+        options.append({"name": "Color","values": color })
   product ={
         "product": {
           "title": name,
           "body_html": description,
           "vendor": brand,
           "product_type": groups,
+          "weight":weight,
 
           "variants": [
             {
@@ -38,20 +94,42 @@ def create_product_object(row):
               "sku": str(sku),
               "barcode" :str(sku),
               "inventory_quantity":int(onHand),
+              "option1":size,
+              "option2":color,
               "tracked":True
             }],
-            "options": [] if size and len(size)==0 else [{"name": "Size","values": [str(size)]}],
+            "options": options,
           "status": "draft"
         }
       }
   products["products"].append(product)
 
+def get_all_groups(barcodes):
+      query="""select distinct 
+            ItemGroupName = STUFF((
+                      SELECT distinct ',' + G.ItemGroupName
+                      FROM itemgroup G
+            JOIN ItemtoGroup IG ON G.ItemGroupID = IG.ItemGroupID
+                      WHERE I.ItemStoreID = IG.ItemStoreID
+                      FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(500)'), 1, 1, '')
+            from 
+            ItemMainAndStoreView I 
+            where I.BarcodeNumber in {}""".format(str(barcodes).replace('[','(').replace(']',')'))
+      cur.execute(query)
+      return cur.fetchone()[0]
+
 def main():
-  cur = select_rows(10)
+  cur = select_rows()
   for row in cur:
     create_product_object(row)
+    groups.update(row[0].split(','))
+  
+  json_groups = {"groups":list(groups)}
+  
+  data = {**products,**json_groups}
+
   with open(mssql_credentials.get('json_name'),'w') as file:
-    json.dump(products,file)
+    json.dump(data,file)
 
 
 main()
